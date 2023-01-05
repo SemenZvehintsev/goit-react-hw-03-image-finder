4f0ea01a5f9153ba9b88e6bea07c0a1227924481
SemenZvehintsev <kvadratniibomzh@gmail.com>
2023-01-05 16:31:11 +0200

    some fixes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -66,15 +66,14 @@ export class App extends Component {
     }
   }
 
-  componentDidUpdate(_, newimages) {
-    if (this.state.images.length > newimages.images.length) {   
+  componentDidUpdate(_, prevstate) {
+    if (this.state.images.length > prevstate.images.length) {   
     const interval = setInterval(() => {
-        console.log(document.documentElement.clientHeight, window.pageYOffset, document.body.offsetHeight)
         window.scrollBy(0, 10)
         if (document.documentElement.clientHeight + window.pageYOffset === document.body.offsetHeight) {
             clearInterval(interval)}
     }, 10)}
-}
+  }
 
   render() {
     const {images, total, search, isLoading, imageId} = this.state;
